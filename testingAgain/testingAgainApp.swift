71b26567d448708539fc875aa40574ccc64b0857
sarah <jisnif2@gmail.com>
2024-06-11 09:51:22 -0500

    Initial Commit

---
+++ testingAgain/testingAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAgainApp.swift
+//  testingAgain
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct testingAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
